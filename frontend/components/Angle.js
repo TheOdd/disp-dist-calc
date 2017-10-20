1dbb3adcab8c5840ebde3b2b7e69ba2217395b0e
TheOdd <owenhines07@gmail.com>
2017-10-20 12:41:56 -0500

    Rounded to 3 digits on all results.
    Imported only round function from mathjs package as to keep size down.

--- frontend/components/Angle.js
+++ frontend/components/Angle.js
@@ -1,7 +1,10 @@
 import React from 'react';
+var core = require('mathjs/core');
+var math = core.create();
+math.import(require('mathjs/lib/function/arithmetic/round'));
 
 const Angle = ({ angle }) => (
-  <h3>Angle: {angle}°</h3>
+  <h3>Angle: {math.round(angle, 3)}°</h3>
 )
 
 export default Angle;
