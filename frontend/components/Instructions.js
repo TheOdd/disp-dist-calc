ece7b4ce6ff0add81d113e6404c4a0a9830b066b
TheOdd <owenhines07@gmail.com>
2017-12-08 10:48:45 -0600

    gh-pages branch creation

--- frontend/components/Instructions.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Instructions = () => (
-  <p>
-    Please insert all applicable numbers into the fields below, and the result will update automatically.
-  </p>
-);
-
-export default Instructions;
--- test.js
+++
@@ -1,3 +0,0 @@
-import test from 'ava';
-
-test.todo('tests for calculating distance, displacement, angle, etc');
